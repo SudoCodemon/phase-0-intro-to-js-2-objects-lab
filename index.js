8f31e8f19d0650d8b8c41b0e838ea843cb88fdf2
Daniel Yang <danyang0724@gmail.com>
2022-05-18 16:07:56 -0400

    commit tree 08c063913ee611b1302f1c532d78c83bce2c0713

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name:"Daniel",
+    streetAddress:"404 birchtree lane"
+}
+
+function updateEmployeeWithKeyAndValue(person, key, value) {
+    const newEmployee = {...person}
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(person, key, value) {
+    person[key] = value;
+    return person;
+}
+
+function deleteFromEmployeeByKey(person, key) {
+    const newEmployee = {...person}
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(person, key) {
+    delete person[key];
+    return person;
+}
